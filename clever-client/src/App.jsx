47a4ba1af1ab92e05a8c3bbbeb3278cc754f052c
valenermel <ermel.valentinet36@gmail.com>
2025-04-03 13:19:00 -0300

    empezar landpage

--- clever-client/src/App.jsx
+++ clever-client/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div id="body">
+      <header>
+        <h1 id="title">Clever</h1>
+        <div id="logo"></div>
+
+      </header>
+      <main>
+        <div id="computerImg"></div>
+        <div id="sloganLoginContainer">
+          <h2 id="slogan">Organiza, comunica, aprende, <br/> Todo en un solo lugar.</h2>
+          <div id="loginRegisterContainer">
+            <button className="loginButton" id="loginButton">Crear cuenta</button>
+            <button className="loginButton" id="registerButton">Iniciar sesión</button>
+          </div>
+        </div>
+      </main>
+
+    </div>
   )
 }
 
 export default App
+
